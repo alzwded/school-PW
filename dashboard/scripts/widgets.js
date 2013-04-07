188f328f3180e41b2d69ee7dd572da1976ee1f57
alzwded <alzwded@gmail.com>
2013-04-07 16:58:03 +0300

    calculator widget sort of working

--- dashboard/scripts/widgets.js
+++ dashboard/scripts/widgets.js
@@ -8,6 +8,9 @@ var movingWidget = null
 var initialX = 0
 var initialY = 0
 
+var cssMap = {}
+var scriptMap = {}
+
 function raiseMe(w) {
 	var container = document.getElementById('content')
 	for(i = 0 ; i < container.children.length ; ++i) {
@@ -21,11 +24,47 @@ function raiseMeCb(e) {
 	raiseMe(d.parentElement)
 }
 
-function new_widget(what) {
+function loadCSSes(csses) {
+	for(i = 0 ; i < csses.length ; ++i) {
+		var me = csses[i].childNodes
+		for(j = 0 ; j < me.length ; ++j) {
+			var href = me[j].nodeValue
+			if(cssMap[href] == null) {
+				var css = document.createElement('link')
+				css.async = false
+				css.setAttribute('rel', 'stylesheet')
+				css.setAttribute('type', 'text/css')
+				css.setAttribute('href', href)
+				cssMap[href] = css
+				document.getElementsByTagName('head')[0].appendChild(css)
+			}
+		}
+	}
+}
+
+function loadScripts(scripts) {
+	for(i = 0 ; i < scripts.length ; ++i) {
+			var href = scripts[i].childNodes[1].childNodes[0].nodeValue
+			var executeCode = scripts[i].childNodes[3].childNodes[0].nodeValue
+			if(scriptMap[href] == null) {
+				var script = document.createElement('script')
+				script.async = false
+				script.setAttribute('type', 'text/javascript')
+				script.setAttribute('src', href)
+				if(executeCode != null) {
+					script.onload = function() { eval(executeCode) }
+				}
+				scriptMap[href] = script
+				document.getElementsByTagName('head')[0].appendChild(script)
+			}
+	}
+}
+
+function new_widget(what, title) {
 	var uniqueName = "__Widget" + lastWidget++
 	// request widget.php
 	var widgetReq = new XMLHttpRequest()
-	var params = "?id=" + uniqueName + "&title=" + what
+	var params = "?id=" + uniqueName + "&title=" + title
 	widgetReq.open("GET", "widget.php" + params, false)
 	widgetReq.send()
 
@@ -34,7 +73,7 @@ function new_widget(what) {
 	widget.id = uniqueName
 	widget.style.left = "100px"
 	widget.style.top = "100px"
-	widget.classList.add("widget")
+	widget.className += " widget"
 	widget.onclick = raiseMeCb
 	widget.innerHTML = widgetReq.responseText
 	document.getElementById('content').appendChild(widget)
@@ -43,10 +82,25 @@ function new_widget(what) {
 	// request path.php
 
 	var appReq = new XMLHttpRequest()
-	appReq.open("GET", what + ".php", false)
+	appReq.open("GET", what, false)
 	appReq.send()
 
-	document.getElementById(uniqueName + "_content").innerHTML = appReq.responseText
+	if(appReq.status == 200) {
+		var csses = appReq.responseXML.getElementsByTagName('css')
+		loadCSSes(csses)
+		var scripts = appReq.responseXML.getElementsByTagName('script')
+		loadScripts(scripts)
+
+		var contentReq = new XMLHttpRequest()
+		contentReq.open("GET", appReq.responseXML.getElementsByTagName('content')[0].childNodes[0].nodeValue, false)
+		contentReq.send()
+		var content = contentReq.responseText
+
+		contentSpan = document.getElementById(uniqueName + "_content")
+		contentSpan.innerHTML = content
+
+	}
+
 
 	return widget
 }
