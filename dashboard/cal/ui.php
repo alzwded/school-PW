844baafc7e35c307ec57872475a3e766ebcc5e55
alzwded <alzwded@gmail.com>
2013-04-09 22:49:00 +0300

    stubs for calendar

--- dashboard/cal/ui.php
+++ dashboard/cal/ui.php
@@ -8,7 +8,21 @@ description: calendar ui
 header('Content-Type: text/html');
 $id = $_GET["id"];
 echo <<<EOT
-<table>
+<table class="calendar_table">
+    <tr><td style="display:block;">
+        <span style="display:block; float:left;">
+            <span class="calendar_button" onclick="prevMonth($id)">&lt;</span>
+            <span id="month_$id"></span>
+            <span class="calendar_button" onclick="nextMonth($id)">&gt;</span>
+        </span>
+        <span style="display:block; float:right;">
+            <span class="calendar_button" onclick="prevYear($id)">&lt;</span>
+            <span id="year_$id"></span>
+            <span class="calendar_button" onclick="nextYear($id)">&gt;</span>
+        </span>
+    </td></tr>
+    <tr><td>
+    </td></tr>
 </table>
 EOT;
 ?>
