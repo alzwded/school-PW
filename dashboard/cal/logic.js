623a8046875611e708d4df911e1c36b13597ab74
alzwded <alzwded@gmail.com>
2013-04-10 20:14:18 +0300

    implement fillWeek
    thanks to stackoverlow

--- dashboard/cal/logic.js
+++ dashboard/cal/logic.js
@@ -38,9 +38,44 @@ function calendar_select(e) {
     alert(fields[0] + ' on ' + fields[1])
 }
 
-function fillWeek(id, w) {
+/* meta:
+    below code (getWeekNumber and comment) are taken from 
+    http://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php
+*/ 
+/* For a given date, get the ISO week number
+ *
+ * Based on information at:
+ *
+ *    http://www.merlyn.demon.co.uk/weekcalc.htm#WNR
+ *
+ * Algorithm is to find nearest thursday, it's year
+ * is the year of the week number. Then get weeks
+ * between that date and the first day of that year.
+ *
+ * Note that dates in one year can be weeks of previous
+ * or next year, overlap is up to 3 days.
+ *
+ * e.g. 2014/12/29 is Monday in week  1 of 2015
+ *      2012/1/1   is Sunday in week 52 of 2011
+ */
+function getWeekNumber(d) {
+    // Copy date so don't modify original
+    d = new Date(d);
+    d.setHours(0,0,0);
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setDate(d.getDate() + 4 - (d.getDay()||7));
+    // Get first day of year
+    var yearStart = new Date(d.getFullYear(),0,1);
+    // Calculate full weeks to nearest Thursday
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7)
+    // Return array of year and week number
+    return [d.getFullYear(), weekNo];
+}
+
+function fillWeek(id, w, d) {
     debug_writeln('TODO fillWeek')
-    document.getElementById('week' + w + '_' + id).innerHTML = w
+    document.getElementById('week' + w + '_' + id).innerHTML = getWeekNumber(d)[1]
 }
 
 function calendar_refresh(id) {
@@ -48,8 +83,9 @@ function calendar_refresh(id) {
     document.getElementById('year_' + id).innerHTML = model[id].year
     var d = new Date(model[id].year, model[id].month, 1, 0, 0, 0, 0)
     var week = 0
+    var aWeekDayDate = null
     var switchWeekFunc = function() {
-        fillWeek(id, week)
+        fillWeek(id, week, aWeekDayDate)
         ++week
     }
     for(var i = 0 ; i < d.getDay() ; ++i) {
@@ -67,6 +103,7 @@ function calendar_refresh(id) {
         td.innerHTML = d.getDate()
         td.name = id + '%|' + d
         td.onclick = calendar_select
+        aWeekDayDate = d
         d.setDate(d.getDate() + 1)
         if(d.getDay() == 0) {
             switchWeekFunc()
@@ -76,6 +113,7 @@ function calendar_refresh(id) {
         var td = document.getElementById('w' + week + 'd' + d.getDay() + '_' + id)
         td.className = 'calendar_otherMonthButton'
         td.innerHTML = d.getDate()
+        aWeekDayDate = d
         d.setDate(d.getDate() + 1)
         if(d.getDay() == 0) {
             debug_writeln(week)
