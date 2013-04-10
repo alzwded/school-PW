c7c6836fddf65fa208dcb21dd2f81b2e1ffb1a08
alzwded <alzwded@gmail.com>
2013-04-10 09:47:18 +0300

    work on calendar

--- dashboard/cal/logic.js
+++ dashboard/cal/logic.js
@@ -3,7 +3,37 @@
 // date: Tue Apr  9 20:42:36 EEST 2013
 // description: calendar functions
 
+var model = new Object()
+var calendar_months = {
+    0: 'January',
+    1: 'February',
+    2: 'March',
+    3: 'April',
+    4: 'May',
+    5: 'June',
+    6: 'July',
+    7: 'August',
+    8: 'September',
+    9: 'October',
+    10: 'November',
+    11: 'December',
+    12: 'January'
+}
+
 function calendar_reset(id) {
+    model[id] = new Object()
+    var now = new Date()
+    model[id].month = now.getMonth()
+    model[id].year = now.getFullYear()
+    model[id].selected = null
+    model[id].callback = function() { calendar_refresh(id); }
+    pim_registerChangeCallback(model[id].callback)
+    calendar_refresh(id)
+}
+
+function calendar_refresh(id) {
+    document.getElementById('month_' + id).innerHTML = calendar_months[model[id].month]
+    document.getElementById('year_' + id).innerHTML = model[id].year
 }
 
 function calendar_clean(id) {
--- dashboard/cal/ui.php
+++ dashboard/cal/ui.php
@@ -21,8 +21,78 @@ echo <<<EOT
             <span class="calendar_button" onclick="nextYear($id)">&gt;</span>
         </span>
     </td></tr>
-    <tr><td>
-    </td></tr>
+    <tr><td><table>
+        <tr>
+            <td class="calendar_headerButton"></td>
+            <td class="calendar_headerButton">M</td>
+            <td class="calendar_headerButton">T</td>
+            <td class="calendar_headerButton">W</td>
+            <td class="calendar_headerButton">T</td>
+            <td class="calendar_headerButton">F</td>
+            <td class="calendar_headerButton">S</td>
+            <td class="calendar_headerButton">S</td>
+        </tr>
+        <tr>
+            <td class="calendar_headerButton" id="week0_$id"></td>
+            <td class="calendar_otherMonthButton" id="w0d0_$id"></td>
+            <td class="calendar_otherMonthButton" id="w0d1_$id"></td>
+            <td class="calendar_otherMonthButton" id="w0d2_$id"></td>
+            <td class="calendar_otherMonthButton" id="w0d3_$id"></td>
+            <td class="calendar_otherMonthButton" id="w0d4_$id"></td>
+            <td class="calendar_otherMonthButton" id="w0d5_$id"></td>
+            <td class="calendar_thisMonthButton" id="w0d6_$id"></td>
+        </tr>
+        <tr>
+            <td class="calendar_headerButton" id="week1_$id"></td>
+            <td class="calendar_otherMonthButton" id="w1d0_$id"></td>
+            <td class="calendar_otherMonthButton" id="w1d1_$id"></td>
+            <td class="calendar_otherMonthButton" id="w1d2_$id"></td>
+            <td class="calendar_otherMonthButton" id="w1d3_$id"></td>
+            <td class="calendar_otherMonthButton" id="w1d4_$id"></td>
+            <td class="calendar_otherMonthButton" id="w1d5_$id"></td>
+            <td class="calendar_otherMonthButton" id="w1d6_$id"></td>
+        </tr>
+        <tr>
+            <td class="calendar_headerButton" id="week2_$id"></td>
+            <td class="calendar_otherMonthButton" id="w2d0_$id"></td>
+            <td class="calendar_otherMonthButton" id="w2d1_$id"></td>
+            <td class="calendar_otherMonthButton" id="w2d2_$id"></td>
+            <td class="calendar_otherMonthButton" id="w2d3_$id"></td>
+            <td class="calendar_otherMonthButton" id="w2d4_$id"></td>
+            <td class="calendar_otherMonthButton" id="w2d5_$id"></td>
+            <td class="calendar_otherMonthButton" id="w2d6_$id"></td>
+        </tr>
+        <tr>
+            <td class="calendar_headerButton" id="week3_$id"></td>
+            <td class="calendar_otherMonthButton" id="w3d0_$id"></td>
+            <td class="calendar_otherMonthButton" id="w3d1_$id"></td>
+            <td class="calendar_otherMonthButton" id="w3d2_$id"></td>
+            <td class="calendar_otherMonthButton" id="w3d3_$id"></td>
+            <td class="calendar_otherMonthButton" id="w3d4_$id"></td>
+            <td class="calendar_otherMonthButton" id="w3d5_$id"></td>
+            <td class="calendar_otherMonthButton" id="w3d6_$id"></td>
+        </tr>
+        <tr>
+            <td class="calendar_headerButton" id="week4_$id"></td>
+            <td class="calendar_otherMonthButton" id="w4d0_$id"></td>
+            <td class="calendar_otherMonthButton" id="w4d1_$id"></td>
+            <td class="calendar_otherMonthButton" id="w4d2_$id"></td>
+            <td class="calendar_otherMonthButton" id="w4d3_$id"></td>
+            <td class="calendar_otherMonthButton" id="w4d4_$id"></td>
+            <td class="calendar_otherMonthButton" id="w4d5_$id"></td>
+            <td class="calendar_otherMonthButton" id="w4d6_$id"></td>
+        </tr>
+        <tr>
+            <td class="calendar_headerButton" id="week5_$id"></td>
+            <td class="calendar_otherMonthButton" id="w5d0_$id"></td>
+            <td class="calendar_otherMonthButton" id="w5d1_$id"></td>
+            <td class="calendar_otherMonthButton" id="w5d2_$id"></td>
+            <td class="calendar_otherMonthButton" id="w5d3_$id"></td>
+            <td class="calendar_otherMonthButton" id="w5d4_$id"></td>
+            <td class="calendar_otherMonthButton" id="w5d5_$id"></td>
+            <td class="calendar_otherMonthButton" id="w5d6_$id"></td>
+        </tr>
+    </table></td></tr>
 </table>
 EOT;
 ?>
