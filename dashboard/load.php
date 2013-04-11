cd5f30ef03c9dc9914f27d10f524304948481a4f
alzwded <alzwded@gmail.com>
2013-04-11 22:10:53 +0300

    add headers for new files

--- dashboard/load.php
+++ dashboard/load.php
@@ -1,3 +1,9 @@
 <?php
+/*
+file: load.php
+author: Vlad Mesco
+date: Thu Apr 11 22:10:03 EEST 2013
+description: puke back a file read locally
+*/
 echo file_get_contents($_FILES["file_data"]["tmp_name"]);
 ?>
--- dashboard/saveAs.php
+++ dashboard/saveAs.php
@@ -1,4 +1,10 @@
 <?php
+/*
+file: saveAs.php
+author: Vlad Mesco
+date: Thu Apr 11 22:10:03 EEST 2013
+description: puke back data as a file which hopefully shows the save as dialog
+*/
 $data = $_POST["data"];
 header("Content-Type: application/x-save-me-as");
 print($data);
