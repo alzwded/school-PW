2c5eee4a00b62ca3339e8180ce7521cee715addf
alzwded <alzwded@gmail.com>
2013-04-13 11:35:48 +0300

    update comment

--- dashboard/load.php
+++ dashboard/load.php
@@ -3,7 +3,7 @@
 file: load.php
 author: Vlad Mesco
 date: Thu Apr 11 22:10:03 EEST 2013
-description: puke back a file read locally
+description: puke back a file read client-side
 */
 echo file_get_contents($_FILES["file_data"]["tmp_name"]);
 ?>
